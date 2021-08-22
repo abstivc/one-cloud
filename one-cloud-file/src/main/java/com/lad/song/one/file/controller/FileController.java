1f214e2eaae31a088beefebd89d2aa0eea794b65
abstivc <13162403691@163.com>
2021-08-22 16:45:28 +0800

    热点控制

--- one-cloud-file/src/main/java/com/lad/song/one/file/controller/FileController.java
+++ one-cloud-file/src/main/java/com/lad/song/one/file/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.lad.song.one.file.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.lad.song.one.common.common.Response;
 import com.lad.song.one.file.service.FileService;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -25,8 +26,8 @@ public class FileController {
         return fileService.queryFileById(id);
     }
 
-    public Response dealFileQuery(String id) {
-        return new Response("990101", "访问人数过多啦!");
+    public Response dealFileQuery(String id, BlockException exception) {
+        return new Response("990101", id + "-> 访问人数过多啦!");
     }
 
 
